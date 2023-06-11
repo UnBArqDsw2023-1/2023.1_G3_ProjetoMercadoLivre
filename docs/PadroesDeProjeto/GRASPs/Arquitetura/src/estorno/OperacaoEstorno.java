738a5b6bdae066dcdc6302a5c027c00b5c062f19
Gabriel Roger Amorim da Cruz <gabrielroger4203@gmail.com>
2023-06-11 20:53:02 -0300

    Adicionando comentarios

--- docs/PadroesDeProjeto/GRASPs/Arquitetura/src/estorno/OperacaoEstorno.java
+++ docs/PadroesDeProjeto/GRASPs/Arquitetura/src/estorno/OperacaoEstorno.java
@@ -2,6 +2,8 @@ package estorno;
 
 import sistema.Pedido;
 
+// Padrão de projeto: Polimorfismo 
+
 public abstract class OperacaoEstorno {
     public abstract String realizarEstorno(Pedido pedido);
 }
