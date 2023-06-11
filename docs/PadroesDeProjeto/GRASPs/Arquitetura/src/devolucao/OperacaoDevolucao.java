883cc8202cbce5c096e9e90ec59cccbf181fa58f
Gabriel Roger Amorim da Cruz <gabrielroger4203@gmail.com>
2023-06-11 20:51:48 -0300

    Adicionando comentarios

--- docs/PadroesDeProjeto/GRASPs/Arquitetura/src/devolucao/OperacaoDevolucao.java
+++ docs/PadroesDeProjeto/GRASPs/Arquitetura/src/devolucao/OperacaoDevolucao.java
@@ -2,6 +2,8 @@ package devolucao;
 
 import sistema.Pedido;
 
+// Padrão de projeto: Polimorfismo 
+
 public abstract class OperacaoDevolucao {
     public abstract String realizarDevolucao(Pedido pedido); 
 }
