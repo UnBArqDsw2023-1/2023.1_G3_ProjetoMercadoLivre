6602e3ede6b73bb523e2c4b2114d54dcbe59e79a
João Pedro de Camargo Vaz <joaopedrocvaz@gmail.com>
2023-06-12 19:48:27 -0300

    Update Devolucao_OutrosProblemas.java

--- docs/PadroesDeProjeto/GRASPs/Arquitetura/src/devolucao/Devolucao_OutrosProblemas.java
+++ docs/PadroesDeProjeto/GRASPs/Arquitetura/src/devolucao/Devolucao_OutrosProblemas.java
@@ -8,7 +8,7 @@ public class Devolucao_OutrosProblemas extends OperacaoDevolucao{
     @Override
     public String realizarDevolucao(Pedido pedido) {
         this.pedido = pedido;        
-        return "##Devolução por item incorreto##\n" + "Pedido Número: " + pedido.getIdPedido() + "\n" +
+        return "##Devolução por outros problemas##\n" + "Pedido Número: " + pedido.getIdPedido() + "\n" +
 		"Quantidade: " + pedido.getQuantidade() + "\n"; // Implementação específica para estorno do comprador
     }
 }
